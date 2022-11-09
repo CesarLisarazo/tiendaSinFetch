7c1d10598261073fa9c04e489cc6331a60978429
Cesar Lisarazo <cesarlisarazo@gmail.com>
2022-11-09 13:53:59 -0300

    tiendaSinFetch

--- script.js
+++ script.js
@@ -1,45 +1,82 @@
 
-
+//Aquí se cargan los datos del Json y se arman las card
 
 fetch('items.json')
 .then ((response) => response.json())
 .then ((items=>items.forEach((items,i)=>{
+
   list.innerHTML += `<li>
+
       <div> ${items.name}</div>
       <div>Precio: $${items.price}</div>
       <image src="${items.image}" />
-      <input type="number" placeholder="Cantidad" onchange='inputChange(${i}, "${items.name}", "${items.price}", "${items.image}")'/>
+      <input type="number" placeholder="Cantidad" onchange='inputChange(${i}, "${items.name}", "${items.price}", "${items.image}", "${items.id}")'/>
       <button>Agregar al carrito</button>
+  
   </li>`
-}) ))
+}
+
+)))
 
 
-cart = []
+//Se declaran variables 
+let cart = []
+let list = document.getElementById('lista')
+let ClearCart = document.getElementById('ClearCart')
+let finalizarCompra = document.getElementById('finalizarCompra')
 
+
+
+//Aquí se carg el contenido del carrito del local storage si es que existe 
 document.addEventListener('DOMContentLoaded', () => {
 cart = JSON.parse(localStorage.getItem('cartShop')) || []
 renderCart()
 })
 
+//boton de finalizar compra
+finalizarCompra.addEventListener('click', ()=> 
+{ 
+if (cart.length==0)
+{
+  swal(" ", "No hay productos para finalizar la compra", "warning");
+}
+else{
+  swal(" ", "Gracias por su compra", "success");
+}})
 
-let list = document.getElementById('lista')
-let ClearCart = document.getElementById('ClearCart')
 
-
-
+//botón para limpiar el carrito
 ClearCart.addEventListener('click', ()=> { 
-cart.length=[]
-localStorage.removeItem('cartShop')
-renderCart()
+if (cart.length== 0)
+{
+swal(" ", "El carrito ya está vacio", "success");
+}
+else
+  {
+  swal({
+  title: "¿Estás seguro?",
+  text: "Estás a punto de vaciar el carrito",
+  icon: "warning",
+  buttons: true,
+  dangerMode: true,
+  })
+.then((willDelete) => {
+if (willDelete) {
+  cart.length=[]
+  localStorage.removeItem('cartShop')
+  renderCart()
+  swal("El carrito se vació", {
+  icon: "success",
+  });
 
-})
+} else {
+        swal(" ", "Podés seguir con tu compra ", "success");
+        }
+ })
+}})
+ 
 
-
-
-
-
-  
-
+//Aquí se renderiza el carrito
 function renderCart() 
 { 
   let cartItems = document.getElementById('carrito')
@@ -52,13 +89,16 @@ function renderCart()
     <div>${item.name}</div>
     <div>Cantidad: ${item.quantity}</div>
     <image src="${item.image}" />
-    </li>`
+    <button onclick ='eliminarProducto(${item.id})'> Eliminar</button>
+     </li>`
   })
   document.getElementById('total').innerHTML = '$' +total
 }
 
 
-function inputChange(i, name, price,image) {
+
+// función invocada para pushear elementos al carrito 
+function inputChange(i, name, price,image,id) {
     let listItem = document.querySelectorAll('li')[i]
     let input = listItem.querySelector('input')
     let button = listItem.querySelector('button')
@@ -71,18 +111,33 @@ function inputChange(i, name, price,image) {
 
 else
 {
-
   button.onclick = function(){
+ 
+  cart.innerHTML = ''
   cart.push({
   quantity: input.value,
   name: name,
   price: price,
-  image: image
+  image: image,
+  id:id
 
-})        
+})
+
   renderCart()
-
   localStorage.setItem('cartShop',JSON.stringify(cart))
-
-
 }}}
+
+
+//función invocada para eliminar elementos del carrito
+function eliminarProducto(id) {
+  cart = cart.filter((el) => el.id != id);
+  localStorage.setItem('cartShop',JSON.stringify(cart))
+  renderCart()
+}
+
+
+
+function existe(){
+console.log(cart)
+
+}
